8344506c9cbe471627cf462e3d5a533358db406e
kevinshurtzacu <kms14d@acu.edu>
2016-09-21 14:36:32 -0500

    Progress made on Course.java

--- Course.java
+++ Course.java
@@ -1,9 +1,23 @@
-/* Course.java
+/* 
+ * Last Modified By: Kevin Shurtz
+ * Last Modified Date: 09/21/2016
+ *
+ * Notes: This will represent a single instance of a student's course.
+ *        The Course object should be aggregated by students. Several 
+ *        instances of this object will eventually be populated with data 
+ *        from a database.
+ * 
+ * Course.java
  */
 
 public class Course
 {
-	public static void main(String[] args) {
-		System.out.println("I'm a course");
-	}
+    // Course characteristics
+    int crn                 // Course Registration Number
+    String coursetitle;     // Course title (no number)
+    int courseNumber;       // Course number (ie. 101, 102, etc.)
+    String courseExt;       
+    
+    
+	public Course()
 }
