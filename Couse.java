71016e7e7c90eca18246b0cbaf75fb900ab9a74f
vxp12c <vxp12c@acu.edu>
2016-09-21 14:20:29 -0500

    Template for Course and Student

---
+++ Couse.java
@@ -0,0 +1,9 @@
+/* Course.java
+ */
+
+public class Course
+{
+	public static void main(String[] args) {
+		System.out.println("I'm a course");
+	}
+}
---
+++ Student.java
@@ -0,0 +1,9 @@
+/* Student.java
+ */
+
+public class Student
+{
+	public static void main(String[] args) {
+		System.out.println("I'm a student");
+	}
+}
